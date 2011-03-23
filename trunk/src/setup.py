652d273996c10f4388211e895e8e3aeeb506d80c
nauzethc@gmail.com <nauzethc@gmail.com@fe68d25d-a3d1-5357-5786-2d9320ad3d51>
2011-03-23 10:19:12 +0000

    Initial Import

---
+++ trunk/src/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+import os.path
+
+def read(filename):
+    return open(os.path.join(os.path.dirname(__file__), filename)).read()
+
+
+VERSION     = '0.1'
+DESCRIPTION = 'Python Spotify API'
+
+setup(
+    name='spotify',
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=read('README'),
+    license='GPL',
+    author='Nauzet Hernandez',
+    author_email='nauzethc@gmail.com',
+    url='http://code.google.com/p/python-spotify/',
+    keywords='spotify api dbus command',
+    packages=['spotify'],
+    platforms=['linux'],
+    classifiers=[
+        "Development Status :: Alpha",
+        "Enviroment :: Console",
+        "Intended Audience :: Developers",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 2",
+        "Topic :: Multimedia :: Sound/Audio :: Players",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+    ],
+)
---
+++ trunk/src/spotify/spotipy.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+# *********************************************************************** #
+# Spotify Python API - Command line
+# Author: Nauzet Hernandez (nauzethc@gmail.com)
+# *********************************************************************** #
+
+from spotify import SpotifyHandler
+from optparse import OptionParser
+
+
+if __name__ == '__main__':    
+
+    spotify_handler = SpotifyHandler()
+
+    parser = OptionParser(usage="usage: %prog [options [arg1]]")
+
+    parser.add_option("-p", "--play-pause",
+        action="callback",
+        callback=spotify_handler.play_pause, 
+        help="Play/Pause current song")
+
+    parser.add_option("-n", "--next",
+        action="callback",
+        callback=spotify_handler.next, 
+        help="Jump to next song")
+
+    parser.add_option("-b", "--previous",
+        action="callback",
+        callback=spotify_handler.previous,
+        help="Jump to previous song")
+
+    parser.add_option("-x", "--stop",
+        action="callback",
+        callback=spotify_handler.stop,
+        help="Stop player")
+
+    parser.add_option("-i", "--info",
+        action="callback",
+        dest="info", type="str",
+        callback=spotify_handler.get_meta,
+        help="Show info for given arg")
+
+    parser.add_option("-s", "--status",
+        action="callback",
+        dest="stat", type="str",
+        callback=spotify_handler.get_property,
+        help="Show status for given arg")
+
+    parser.add_option("-c", "--cover-path",
+        action="callback",
+        callback=spotify_handler.fetch_cover,
+        help="Get cached cover image path")
+
+    (options, args) = parser.parse_args()
